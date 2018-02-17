ab4d6087c76ccaa5758b19b1a9560cc332b8b2ec
ilya682003 <ilyanfs682003@mail.ru>
2018-02-17 14:01:02 +0300

    ООП

---
+++ login/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once "libs/functions.php";
+require_once "users.php";
+require_once "menu.php";
+require_once "routes.php";
---
+++ login/routes.php
@@ -0,0 +1,5 @@
+<?php
+if ($_GET['page']=="autorisation")
+{
+    require_once "vievs/autorisation_form.php";
+}
