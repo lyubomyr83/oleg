fa591a36818505342523a190bd9883427c297b23
ilya682003 <ilyanfs682003@mail.ru>
2018-08-25 13:59:43 +0300

    admin zone

--- admin/views/VPageEdit.php
+++ admin/views/VPageEdit.php
@@ -5,3 +5,9 @@ $page = $editPage->editPage($_GET["id"]);
 <h2>
     Редактирование страницы <?=$page['menu_name']?>
 </h2>
+<form method="post">
+    title<input type="text" name="title" value="<?=$page['title']?>">
+    иконка<input type="text" name="menu_icon" value="<?=$page['menu_icon']?>">
+    название в меню<input type="text" name="menu_name" value="<?=$page['menu_name']?>">
+    контент
+</form>
